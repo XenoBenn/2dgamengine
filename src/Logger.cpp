494f238feda2623d24d23e53a113a3ee006f1f84
XenoBenn <benjamin.komar@gmail.com>
2023-02-10 06:32:43 +0100

    simple logger implementation

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,48 +1,30 @@
 #include "Logger.h"
 #include <iostream>
-#include <stdlib.h>
+#include <string.h>
 #include <ctime>
+#include <chrono>
 
+std::vector<LogEntry> Logger::messages;
+
+std::string CurrentDateTimeToString(){
+    std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::string output(30, '\0');
+    std::strftime(&output[0], output.size(), "%d-%b-%Y %H:%M:%S", std::localtime(&now));
+    return output;
+}
 
 void Logger::Log(const std::string& message) {
-    //TODO:
-    //Print on the console the message:
-    //LOG: [ DATE  TIME ] - Here goes the message...
-    //This should be in green
-    std::time_t tmNow;
-    tmNow = time(NULL);
-    struct tm t = *localtime(&tmNow);
-    
-    system("Color 0A");
-    std::cout << "[" << t.tm_mday << "/" << t.tm_mon +1 << "/" << t.tm_year + 1900 << "]" << "[" << t.tm_hour << ":" << t.tm_min<< ":" << t.tm_sec<< "]"<< " - " << std::endl;
-
+    LogEntry logEntry;
+    logEntry.type = LOG_INFO;
+    logEntry.message = "LOG: [" + CurrentDateTimeToString() + "]: " + message;
+    std::cout << "\x1B[32m" << logEntry.message << "\033[0m" << std::endl;  
+    messages.push_back(logEntry); 
 }
 
 void Logger::Err(const std::string& message) {
-    //TODO:
-    //Print on the console the message:
-    //ERR: [ DATE  TIME ] - Here goes the message...
-    //This should be in red
-    std::time_t tmNow;
-    tmNow = time(NULL);
-    struct tm t = *localtime(&tmNow);
-    
-    system("Color 0A");
-    std::cout   << "[" 
-                << t.tm_mday 
-                << "/" 
-                << t.tm_mon +1 
-                << "/" 
-                << t.tm_year + 1900 
-                << "]" 
-                << "[" 
-                << t.tm_hour 
-                << ":" 
-                << t.tm_min
-                << ":"
-                << t.tm_sec
-                << "]"
-                << " - "
-                << std::endl;
-
+    LogEntry logEntry;
+    logEntry.type = LOG_ERROR;
+    logEntry.message = "ERR: [" + CurrentDateTimeToString() + "]: " + message;
+    std::cout << "\x1B[91m" << logEntry.message << "\033[0m" << std::endl;  
+    messages.push_back(logEntry);  
 }
--- src/Logger.h
+++ src/Logger.h
@@ -1,11 +1,23 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <vector>
 #include <string>
 
+enum LogType {   
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR
+};
 
-class Logger{
+struct LogEntry {
+    LogType type;
+    std::string message;
+};
+
+class Logger {
     public:
+    static std::vector<LogEntry> messages;
     static void Log(const std::string& message);
     static void Err(const std::string& message);
 };
