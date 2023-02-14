4a80d2e365a8ef9c77fbad48a083f6ef4b6dac9d
XenoBenn <benjamin.komar@gmail.com>
2023-02-14 13:59:58 +0100

    operator overloading

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -35,7 +35,15 @@ class Entity {
 
     public:
         Entity(int id): id(id) {};
+        Entity(const Entity& entity) = default;
         int GetId() const;
+
+        Entity& operator = (const Entity& other) = default;
+        bool operator == (const Entity& other) const { return id == other.id; }
+        bool operator != (const Entity& other) const { return id != other.id; }
+        bool operator > (const Entity& other) const { return id > other.id; }
+        bool operator < (const Entity& other) const { return id < other.id; }
+
 };
 
 ///////////////////////////////////////////////////////////////////
